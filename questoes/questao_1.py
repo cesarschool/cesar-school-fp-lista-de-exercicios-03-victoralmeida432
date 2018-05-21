df711ee961bad2557c37f066a275f42763921019
victoralmeida432 <38266337+victoralmeida432@users.noreply.github.com>
2018-05-21 19:02:09 -0300

    Update questao_1.py

--- questoes/questao_1.py
+++ questoes/questao_1.py
@@ -28,18 +28,17 @@
 # substituindo apenas o comando print(questão...) existente.
 ##
 def main():
-
-banco_de_dados = []
-senha = str(input('Digite uma senha: '))
-registro = []
-while senha:
-    registro.append(senha)
-    if 6 <= len(senha) <= 12:
-        for i in range(0, len(senha) - 1):
-            if senha[i].isalnum() or senha[i].isupper() or senha[i].islower() or senha[i].isspace():
-                continue
-            if ('@' in senha) or ('#' in senha) or ('$' in senha):
-                banco_de_dados.append(senha)
+    banco_de_dados = []
+    senha = str(input('Digite uma senha: '))
+    registro = []
+    while senha:
+        registro.append(senha)
+        if 6 <= len(senha) <= 12:
+            for i in range(0, len(senha) - 1):
+                if senha[i].isalnum() or senha[i].isupper() or senha[i].islower() or senha[i].isspace():
+                    continue
+                if ('@' in senha) or ('#' in senha) or ('$' in senha):
+                    banco_de_dados.append(senha)
     senha = str(input('Digite uma senha: '))
 print(banco_de_dados)
     
